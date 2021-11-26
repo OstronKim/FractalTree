554b4f61dde8871aa99dcb886f028170e7d6fe88
Arvid Magnusson <arvidmagnusson97@gmail.com>
2021-11-26 17:44:19 +0100

    Implemented adjustable parameters

--- branch.js
+++ branch.js
@@ -1,22 +1,31 @@
-function branch(depth, len) {
-  let rot = [PI / 4, -PI / 4];
-  let scaleFactor = 0.75;
+function branch(depth, len, rseed) {
+  randomSeed(rseed);
+
   if (depth > maxDepth) {
     let leaf = new Leaf();
     leaf.display();
     return;
   }
 
+  let rot = [];
+  let scaleFactor = 0.75;
+
   push();
+  strokeWeight(8 * Math.pow((maxDepth - depth + 1) / maxDepth, 2));
   line(0, 0, 0, -len);
 
   push();
   translate(0, -len);
 
+  rot[0] = rotSlider.value() * (1 + rand2());
+  rot[1] = -rotSlider.value() * (1 - rand2());
+  rot[2] = rotSlider.value() / 2 + rand2();
+
   for (let i = 0; i < nrBranches; i++) {
+    let randSeed = random(1000);
     push();
     rotate(rot[i]);
-    branch(depth + 1, len * scaleFactor);
+    branch(depth + 1, len * scaleFactor, randSeed);
     pop();
   }
 
--- main.js
+++ main.js
@@ -1,18 +1,35 @@
 let leaves = [];
 let counter = 0;
+let seed = 80;
 let maxDepth;
 let startLength;
 let nrBranches;
 
+//Sliders
 let maxDepthSlider;
+let rotSlider;
+
+//Sliderlabels
+let maxDepthLabel;
+let rotLabel;
+
+//Buttons
 let generate;
 
 function setup() {
-  let cnv = createCanvas(900, 600);
-  //cnv.position(200, 20);
+  createCanvas(windowWidth, windowHeight);
 
-  maxDepthSlider = createSlider(0, 10, 5);
-  maxDepthSlider.position(width / 2, height + 20);
+  //sliders (Lägg i nån funktion?, typ createSLiders())
+  maxDepthSlider = createSlider(0, 12, 9);
+  maxDepthSlider.position(10, 20);
+  //Labels i draw function antagligen
+  maxDepthLabel = createSpan("Max depth: " + maxDepthSlider.value());
+  maxDepthLabel.position(150, 20);
+
+  rotSlider = createSlider(PI / 30, PI / 3, PI / 6, PI / 2 / 50);
+  rotSlider.position(10, 40);
+  rotLabel = createSpan("Branch angle: " + rotSlider.value().toFixed(3));
+  rotLabel.position(150, 40);
 
   generate = createButton("generate");
   generate.position(width / 2 - 80, height + 20);
@@ -30,6 +47,14 @@ function draw() {
   maxDepth = maxDepthSlider.value();
   let depth = 1;
   stroke(255);
-  translate(width / 2, height);
-  branch(depth, startLength);
+  translate(width / 2, height - 50);
+  branch(depth, startLength, seed);
+}
+
+function rand1() {
+  return random(0.8, 1.2);
+}
+//Random float point between -1,1
+function rand2() {
+  return random(2) - 1;
 }
